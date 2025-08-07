2b95e1fd22eb50e14547737c8d8fa97d7e371893
Jorge Cuervo <jorgecuervo527@gmail.com>
2025-08-06 19:01:54 -0500

    Add files via upload

---
+++ api/api/src/main/java/med/voll/api/domain/medico/DatosDetalleMedico.java
@@ -0,0 +1,25 @@
+package med.voll.api.domain.medico;
+
+import med.voll.api.domain.direccion.Direccion;
+
+public record DatosDetalleMedico(
+        long id,
+        String nombre,
+        String email,
+        String documento,
+        String telefono,
+        Especialidad especialidad,
+        Direccion direccion
+) {
+    public DatosDetalleMedico(Medico medico) {
+        this(
+                medico.getId(),
+                medico.getNombre(),
+                medico.getEmail(),
+                medico.getDocumento(),
+                medico.getTelefono(),
+                medico.getEspecialidad(),
+                medico.getDireccion()
+        );
+    }
+}
---
+++ api/api/target/classes/db/migration/V3__alter-table-medicos-add-column-telefono.sql
@@ -0,0 +1,2 @@
+alter table medicos
+    add column telefono varchar(20) not null;
